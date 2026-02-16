dc31a57ec233bfa1234878a6341823bb6d1e1480
Pierre-Luc Robitaille <pierreluc2002@gmail.com>
2026-02-16 01:12:09 -0500

    Should have done the basis for the card struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,83 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+use std::env;
+
+use bytes::Bytes;
+use http_body_util::{BodyExt, Empty};
+use models::cards;
+use regex::Regex;
+use serde::Deserialize;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+struct Handler;
+
+const SCRYFALL_API: &'static str = "https://api.scryfall.com";
+const CARDS_SEARCH: &'static str = "/cards/search";
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+struct CardSearch {
+    pub q: String,
+    pub unique: Option<String>,
+    pub order: Option<String>,
+    pub dir: Option<String>,
+    pub include_extras: Option<bool>,
+    pub include_multilingual: Option<bool>,
+    pub include_variations: Option<bool>,
+    pub page: Option<i32>,
+    pub format: Option<String>,
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let re = Regex::new(r"\[\[(.*)\]\]").unwrap();
+        let Some(caps) = re.captures(&msg.content) else {
+            return;
+        };
+        if let Err(why) = msg
+            .channel_id
+            .say(&ctx.http, format!("CardName: {}", &caps[1]))
+            .await
+        {
+            println!("Error sending message: {why:?}");
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+async fn fetch_json(url: hyper::Uri) {}
+
+#[tokio::main]
+async fn main() {
+    // Configure the client with your Discord bot token in the environment.
+    #[cfg(debug_assertions)]
+    {
+        let _ = loadenv::load();
+    }
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    // Create a new instance of the Client, logging in as a bot. This will automatically prepend
+    // your bot token with "Bot ", which is a requirement by Discord for bot users.
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform exponential backoff until
+    // it reconnects.
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
---
+++ src/models/cards.rs
@@ -0,0 +1,249 @@
+use chrono::NaiveDate;
+use hyper::Uri;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
+use uuid::Uuid;
+
+pub struct Card {
+    pub arena_id: Option<i32>,
+    pub id: Uuid,
+    pub land: String,
+    pub mtgo_id: Option<i32>,
+    pub mtgo_foil_id: Option<i32>,
+    pub multiverse_ids: Option<Vec<i32>>,
+    pub resource_id: Option<String>,
+    pub tcgplayer_id: Option<i32>,
+    pub tcgplayer_etched_id: Option<i32>,
+    pub cardmarket_id: Option<i32>,
+    pub object: String,
+    pub layout: String,
+    pub oracle_id: Option<Uuid>,
+    pub prints_search_uri: Uri,
+    pub ruling_uri: Uri,
+    pub scryfall_uri: Uri,
+    pub uri: Uri,
+    pub all_parts: Option<Vec<RelatedCardObjects>>,
+    pub card_faces: Option<Vec<CardFace>>,
+    pub cmc: f32,
+    pub color_identity: Colors,
+    pub color_indicator: Option<Colors>,
+    pub defense: Option<String>,
+    pub edhrec_rank: Option<i32>,
+    pub game_changer: Option<bool>,
+    pub hand_modifier: Option<String>,
+    pub keywords: Vec<String>,
+    pub legalities: HashMap<String, Legalities>,
+    pub life_modifier: Option<String>,
+    pub loyalty: Option<String>,
+    pub mana_cost: Option<String>,
+    pub name: String,
+    pub oracle_text: Option<String>,
+    pub penny_rank: Option<i32>,
+    pub power: Option<String>,
+    pub produced_mana: Option<Colors>,
+    pub reserved: bool,
+    pub toughness: Option<String>,
+    pub type_line: String,
+    pub artist: Option<String>,
+    pub artist_ids: Option<Vec<Uuid>>,
+    pub attraction_lights: Option<Vec<i32>>,
+    pub booster: bool,
+    pub border_color: BorderColor,
+    pub card_back_id: Uuid,
+    pub collector_number: String,
+    pub content_warning: Option<bool>,
+    pub digital: bool,
+    pub finishes: Vec<Finishes>,
+    pub flavor_name: Option<String>,
+    pub flavor_text: Option<String>,
+    pub frame_effects: Option<Vec<FrameEffects>>,
+    pub frame: Frames,
+    pub full_art: bool,
+    pub games: Vec<Games>,
+    pub highres_image: bool,
+    pub illustration_id: Option<Uuid>,
+    pub image_status: ImageStatuses,
+    pub image_uris: Vec<HashMap<String, Value>>,
+    pub oversized: bool,
+    pub prices: Vec<HashMap<String, i32>>,
+    pub printed_name: Option<String>,
+    pub printed_text: Option<String>,
+    pub printed_type_line: Option<String>,
+    pub promo: bool,
+    pub promo_types: Option<Vec<String>>,
+    pub purchase_uris: Option<Vec<HashMap<String, Value>>>,
+    pub rarity: Rarities,
+    pub related_uris: Vec<HashMap<String, Value>>,
+    pub released_at: NaiveDate,
+    pub reprint: bool,
+    pub scryfall_set_uri: Uri,
+    pub set_name: String,
+    pub set_search_uri: Uri,
+    pub set_type: String,
+    pub set_uri: Uri,
+    pub set: String,
+    pub set_id: Uuid,
+    pub story_spotlight: bool,
+    pub textless: bool,
+    pub variation: bool,
+    pub variation_of: Option<Uuid>,
+    pub security_stamp: Option<String>,
+    pub watermark: Option<String>,
+    pub preview: Option<Preview>,
+}
+
+pub struct Preview {
+    pub previewed_at: NaiveDate,
+    pub source_uri: Uri,
+    pub source: String,
+}
+
+pub struct CardFace {
+    pub artist: Option<String>,
+    pub artist_id: Option<String>,
+    pub cmc: Option<f32>,
+    pub color_indicator: Option<Colors>,
+    pub colors: Option<Colors>,
+    pub defense: Option<String>,
+    pub flavor_text: Option<String>,
+    pub illustration_id: Option<Uuid>,
+    pub image_uris: Option<Vec<HashMap<String, Value>>>,
+    pub layout: Option<String>,
+    pub loyalty: Option<String>,
+    pub mana_cost: String,
+    pub name: String,
+    pub object: String,
+    pub oracle_id: Option<Uuid>,
+    pub oracle_text: Option<String>,
+    pub power: Option<String>,
+    pub printed_name: Option<String>,
+    pub printed_type_line: Option<String>,
+    pub thoughness: Option<String>,
+    pub type_line: Option<String>,
+    pub watermark: Option<String>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Legalities {
+    NotLegal,
+    Legal,
+    Restricted,
+    Banned,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+pub enum Colors {
+    #[serde(rename = "W")]
+    White,
+    #[serde(rename = "U")]
+    Blue,
+    #[serde(rename = "B")]
+    Black,
+    #[serde(rename = "G")]
+    Green,
+    #[serde(rename = "R")]
+    Red,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum BorderColor {
+    Silver,
+    Black,
+    White,
+    Borderless,
+    Yellow,
+    Gold,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Finishes {
+    Foil,
+    NonFoil,
+    Etched,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Games {
+    Paper,
+    Arena,
+    MTGO,
+    Astral,
+    Sega,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ImageStatuses {
+    Missing,
+    Placeholder,
+    Lowres,
+    HighresScan,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Rarities {
+    Common,
+    Uncommon,
+    Rare,
+    Special,
+    Mythic,
+    Bonus,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum FrameEffects {
+    Legendary,
+    Miracle,
+    Enchantment,
+    Draft,
+    Devoid,
+    Tombstone,
+    Colorshifted,
+    Inverted,
+    SunMoonDFC,
+    CompassLandDFC,
+    OriginPWDFC,
+    MoonEldraziDFC,
+    WaxingAndWaningMoonDFC,
+    Showcase,
+    ExtendedArt,
+    Companion,
+    Etched,
+    Snow,
+    Lesson,
+    ShatteredGlass,
+    ConvertDFC,
+    FanDFC,
+    UpsideDownDFC,
+    Spree,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Frames {
+    #[serde(rename = "1993")]
+    _1993,
+    #[serde(rename = "1997")]
+    _1997,
+    #[serde(rename = "2003")]
+    _2003,
+    #[serde(rename = "2015")]
+    _2015,
+    Future,
+}
+
+pub struct RelatedCardObjects {
+    pub id: Uuid,
+    pub object: String,
+    pub component: String,
+    pub name: String,
+    pub type_line: String,
+    pub uri: Uri,
+}
